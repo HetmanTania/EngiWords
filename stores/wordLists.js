79cf2390fe65f0b43827d04c3ec24e36af1660d9
HetmanTania <hetman.1007@gmail.com>
2024-10-10 18:03:43 +0300

    test

--- stores/wordLists.js
+++ stores/wordLists.js
@@ -21,7 +21,7 @@ export const useWordListsStore = defineStore('wordLists', () => {
             })
 
         } catch (e) {
-            console.log(e)
+            console.log(e);
         }
     }
 
